40d7fed72256bd99f7cb907bedd75beb5e41d370
Aldian FN <Aldian FN@DESKTOP-8UG84AI>
2019-02-28 23:28:48 +0700

    First Commit

---
+++ pv03_1700635_AldianFN/pv03_1700635_AldianFN/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace pv03_1700635_AldianFN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNim = new System.Windows.Forms.Button();
+            this.txtNim = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtKategori = new System.Windows.Forms.TextBox();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtIdTodo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtNamaTodo = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtKategoriTodo = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(557, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "To Do List";
+            // 
+            // btnNim
+            // 
+            this.btnNim.Location = new System.Drawing.Point(692, 111);
+            this.btnNim.Name = "btnNim";
+            this.btnNim.Size = new System.Drawing.Size(82, 31);
+            this.btnNim.TabIndex = 1;
+            this.btnNim.Text = "&Ok";
+            this.btnNim.UseVisualStyleBackColor = true;
+            this.btnNim.Click += new System.EventHandler(this.btnNim_Click);
+            // 
+            // txtNim
+            // 
+            this.txtNim.Location = new System.Drawing.Point(522, 116);
+            this.txtNim.Name = "txtNim";
+            this.txtNim.Size = new System.Drawing.Size(133, 26);
+            this.txtNim.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(559, 371);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tambah To Do";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(403, 441);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nama Kegiatan";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(741, 441);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(168, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Kategori Kegiatan";
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(408, 484);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(133, 26);
+            this.txtNama.TabIndex = 6;
+            // 
+            // txtKategori
+            // 
+            this.txtKategori.Location = new System.Drawing.Point(746, 484);
+            this.txtKategori.Name = "txtKategori";
+            this.txtKategori.Size = new System.Drawing.Size(133, 26);
+            this.txtKategori.TabIndex = 7;
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Location = new System.Drawing.Point(587, 540);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(116, 37);
+            this.btnTambah.TabIndex = 8;
+            this.btnTambah.Text = "Tambahkan";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            this.btnTambah.Click += new System.EventHandler(this.btnTambah_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(283, 170);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(730, 156);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(700, 722);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(116, 37);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Hapus";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(479, 722);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(133, 26);
+            this.txtId.TabIndex = 13;
+            this.txtId.TextChanged += new System.EventHandler(this.txtId_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(474, 679);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Id";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(569, 617);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(127, 25);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Delete To Do";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(914, 899);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 37);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "&Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtIdTodo
+            // 
+            this.txtIdTodo.Location = new System.Drawing.Point(324, 904);
+            this.txtIdTodo.Name = "txtIdTodo";
+            this.txtIdTodo.Size = new System.Drawing.Size(133, 26);
+            this.txtIdTodo.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(319, 861);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 25);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Id";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(569, 799);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(134, 25);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Update To Do";
+            // 
+            // txtNamaTodo
+            // 
+            this.txtNamaTodo.Location = new System.Drawing.Point(522, 904);
+            this.txtNamaTodo.Name = "txtNamaTodo";
+            this.txtNamaTodo.Size = new System.Drawing.Size(133, 26);
+            this.txtNamaTodo.TabIndex = 21;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(517, 861);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(147, 25);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Nama Kegiatan";
+            // 
+            // txtKategoriTodo
+            // 
+            this.txtKategoriTodo.Location = new System.Drawing.Point(700, 904);
+            this.txtKategoriTodo.Name = "txtKategoriTodo";
+            this.txtKategoriTodo.Size = new System.Drawing.Size(133, 26);
+            this.txtKategoriTodo.TabIndex = 23;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(695, 861);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(168, 25);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Kategori Kegiatan";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1285, 1050);
+            this.Controls.Add(this.txtKategoriTodo);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtNamaTodo);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtIdTodo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.txtKategori);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNim);
+            this.Controls.Add(this.btnNim);
+            this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(1307, 1078);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Scroll += new System.Windows.Forms.ScrollEventHandler(this.Enable);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNim;
+        private System.Windows.Forms.TextBox txtNim;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtKategori;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtIdTodo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtNamaTodo;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtKategoriTodo;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ pv03_1700635_AldianFN/pv03_1700635_AldianFN/Form1.cs
@@ -0,0 +1,137 @@
+﻿using pv03_1700635_AldianFN.Model;
+using pv03_1700635_AldianFN.Repository;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pv03_1700635_AldianFN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnNim_Click(object sender, EventArgs e)
+        {
+            TodoRepository todo = new TodoRepository();
+            
+            string nim = txtNim.Text;
+
+            if(todo.cekNim(nim) == 1)
+            {
+                txtNim.Enabled = false;
+                btnTambah.Enabled = true;
+                btnNim.Enabled = false;
+
+                dataGridView1.DataSource = todo.getByNim(nim);
+
+            }
+            else
+            {
+                MessageBox.Show("NIM tidak terdaftar");
+                txtNim.Text = "";
+                txtNim.Focus();
+            }
+            
+        }
+
+        private void btnTambah_Click(object sender, EventArgs e)
+        {
+            Todo temp = new Todo();
+
+            temp.NimMhs = txtNim.Text;
+            temp.Nama = txtNama.Text;
+            temp.Kategori = txtKategori.Text;
+
+            TodoRepository todo = new TodoRepository();
+
+            todo.addTodo(temp);
+
+            string nim = txtNim.Text;
+
+            btnDelete.Enabled = true;
+            btnTambah.Enabled = false;
+
+            dataGridView1.DataSource = todo.getByNim(nim);
+        }
+
+        private void txtId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            Todo temp = new Todo();
+
+            temp.Id= Convert.ToInt16(txtId.Text);
+
+            TodoRepository todo = new TodoRepository();
+
+            todo.deleteTodo(temp);
+
+            string nim = txtNim.Text;
+
+            btnDelete.Enabled = false;
+
+            dataGridView1.DataSource = todo.getByNim(nim);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Todo temp = new Todo();
+
+            temp.Id = Convert.ToInt16(txtIdTodo.Text);
+            temp.Nama = txtNamaTodo.Text;
+            temp.Kategori = txtKategoriTodo.Text;
+
+            TodoRepository todo = new TodoRepository();
+
+            todo.updateTodo(temp);
+
+            string nim = txtNim.Text;
+            
+            button1.Enabled = false;
+            txtIdTodo.Enabled = false;
+
+            dataGridView1.DataSource = todo.getByNim(nim);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Enable(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtId.Text = dataGridView1.CurrentRow.Cells["Id"].Value.ToString();
+            txtIdTodo.Text = dataGridView1.CurrentRow.Cells["Id"].Value.ToString();
+            txtNamaTodo.Text = dataGridView1.CurrentRow.Cells["Nama"].Value.ToString();
+            txtKategoriTodo.Text = dataGridView1.CurrentRow.Cells["Kategori"].Value.ToString();
+        }
+    }
+}
